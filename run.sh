b2d10e88a441d5025412d5ece12c76608d488fd4
Rozek <vboxuser@Xubuntu.myguest.virtualbox.org>
2022-12-03 09:44:20 +0100

    Calculator v0.1

--- run.sh
+++ run.sh
@@ -2,14 +2,3 @@
 rm ./bin/main
 g++ ./src/main.cpp -o ./bin/main &&  
 ./bin/main
-EOF 
-sudo chmod +x ./run.sh
-
-
-touch ./src/main.cpp
-cat <<- 'EOF' > ./src/main.cpp
-#include <iostream>
-int main ()
-{
-
-}
