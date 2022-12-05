62106ec9cfd8b86ad73be23601b6ba1d30d71285
Rozek <vboxuser@Xubuntu.myguest.virtualbox.org>
2022-12-05 18:21:14 +0100

    Calculator v0.3 W tej wersji skrocona zostal kod do jednej lini printujacej wynik.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main ()
+int main()
 {
     float num1, num2;
     char operation;
+    double wynik;
     std::cout << "** Zajebisty kalkulator **" << endl;
-    std::cin >> num1 >> operation >>num2;
+    std::cin >> num1 >> operation >> num2;
 
+
+    /* To jest moja orginalna czesc programu
         switch (operation)
             {
+
+
                 case '-': std::cout << num1 << operation << num2 << "-" << num1 - num2 << std::endl; break;
                 case '+': std::cout << num1 << operation << num2 << "+" << num1 + num2 << std::endl; break;
                 case '/': std::cout << num1 << operation << num2 << "/" << num1 / num2 << std::endl; break;
                 case '*': std::cout << num1 << operation << num2 << "*" << num1 * num2 << std::endl; break;
-                
+
+                default: cout << "Niewlasciwa komenda" << endl;
+            }
+    */
+    switch (operation)
+            {
+
+
+                case '-': wynik = num1 - num2; break;
+                case '+': wynik = num1 + num2; break;
+                case '/': wynik = num1 / num2; break;
+                case '*': wynik = num1 * num2; break;
+
                 default: cout << "Niewlasciwa komenda" << endl;
             }
 
+            std::cout << num1 << operation << num2 << "  To jest kurwa wynik:" << wynik << std::endl; 
 
-    return 0;   
 
+
+    return 0;
 }
+
