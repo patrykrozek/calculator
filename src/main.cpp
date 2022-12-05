e24bd9873559a45b0e47a0f8305706649a02a4b4
Rozek <vboxuser@Xubuntu.myguest.virtualbox.org>
2022-12-05 17:47:29 +0100

    Calculator v0.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+using namespace std;
+
 int main ()
 {
+    float num1, num2;
+    char operation;
+    std::cout << "** Zajebisty kalkulator **" << endl;
+    std::cin >> num1 >> operation >>num2;
+
+        switch (operation)
+            {
+                case '-': std::cout << num1 << operation << num2 << "-" << num1 - num2 << std::endl; break;
+                case '+': std::cout << num1 << operation << num2 << "+" << num1 + num2 << std::endl; break;
+                case '/': std::cout << num1 << operation << num2 << "/" << num1 / num2 << std::endl; break;
+                case '*': std::cout << num1 << operation << num2 << "*" << num1 * num2 << std::endl; break;
+                
+                default: cout << "Niewlasciwa komenda" << endl;
+            }
+
+
+    return 0;   
+
 }
