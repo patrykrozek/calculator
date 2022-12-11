b29882fcbc25a537e938a3a2b53abbb6e7f9f1ec
Rozek <vboxuser@Xubuntu.myguest.virtualbox.org>
2022-12-11 10:55:15 +0100

    Calculator v1.0 Here I modified code to work on functions. It works nice and I understand it now.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
+double add(double a,double b) // Sygnatura funkcji
+{
+    
+    return a + b;
+}
+double min(double num1,double num2) // Sygnatura funkcji
+{
+    return num1 - num2;
+}
+double multiply(double num1,double num2) // Sygnatura funkcji
+{
+    return num1 * num2;
+}
+double divide(double num1,double num2) // Sygnatura funkcji
+{
+    return num1 / num2;
+}
+void printer(double a,double b,char op,double w) // Sygnatura funkcji
+{
+    std::cout << a << op << b << "  To jest kurwa wynik:" << w << std::endl;
+}
+
 int main()
 {
     float num1, num2;
+    
     char operation;
     double wynik;
     std::cout << "** Zajebisty kalkulator **" << endl;
@@ -27,16 +50,16 @@ int main()
             {
 
 
-                case '-': wynik = num1 - num2; break;
-                case '+': wynik = num1 + num2; break;
-                case '/': wynik = num1 / num2; break;
-                case '*': wynik = num1 * num2; break;
+                case '-': wynik = min(num1,num2); break;
+                case '+': wynik = add(num1,num2); break;
+                case '/': wynik = divide(num1,num2); break;
+                case '*': wynik = multiply(num1,num2); break;
 
                 default: cout << "Niewlasciwa komenda" << endl;
             }
 
-            std::cout << num1 << operation << num2 << "  To jest kurwa wynik:" << wynik << std::endl; 
-
+            printer(num1,num2,operation,wynik);
+cout <<num2; 
 
 
     return 0;
